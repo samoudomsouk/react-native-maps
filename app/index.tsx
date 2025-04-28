63da0fb75fd44622164bb3fa42d3761bc742b3cb
Samu <samoudomsouk@gmail.com>
2025-04-29 02:02:38 +0700

    Installed  and utilize basic

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <MapView style={{ flex: 1, width: "100%", height: "100%" }} />
     </View>
   );
 }
